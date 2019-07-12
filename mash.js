ab6cd30576803204b4fbbddb3331943d515b5f75
filiptASC6 <filipjamest@gmail.com>
2019-07-12 15:57:01 -0400

    MASH Game

--- mash.js
+++ mash.js
@@ -1,13 +1,11 @@
 
 var home = ["a Mansion", "an Apartment" , "a Shack", "a House", "a Dumpster", "your Parents' Basement"];
 
-var vehicle = ["Lambo", "Tesla", "Horse", "Wagon","SUV", "Submarine", "Cardboard Box"]
+var vehicle = ["in a Lambo", "in a Tesla", "on a Horse", "in a Wagon","in a SUV", "inside a Submarine", "in a Cardboard Box"]
 
 const spouseName = ["Karen", "Sophia", "Shaniqua", "Trish", "Oliva", "Emma", "Grace", "Hannah"]
 
-const job = ["Engineer", "Stunt Double", "Doctor", "Librarian", "Soldier"]
-
-
+const jobs = ["Engineer", "Stunt Double", "Doctor", "Librarian", "Soldier", "Teacher", "Disney World Employee", "Janitor"]
 
 function getHome () {
     return (home[Math.floor(Math.random()*home.length)]);
@@ -70,14 +68,20 @@ function spouse(){
     return ((spouseName[Math.floor(Math.random()*spouseName.length)]));
 }
 
+function job(){
+    return ((jobs[Math.floor(Math.random()*jobs.length)]));
+}
 
 
 
 function mash() {
-    return "You live in " + homeSeventyFive() + ", and you have " + fiftyFifty() + " kids with the woman you married," + spouse() +"!" + " Together with your family you ride a " + getCar() + "! " + "The neighbors are very jealous of your vehicle.";
+    return "You live in " + homeSeventyFive() + ", and you have " + fiftyFifty() + " kids with the woman you married, " + spouse() +"!" + " Together with your family you ride " + getCar() + "! ";
 }
 
+function mash2(){
+    return "The neighbors are very jealous of your vehicle." +" "+ "You work as a " + job() + ". " + "It took forever to get that job, but you finally made it, the hard work paid off."
+}
 
 console.log (mash());
-
+console.log (mash2());
 
